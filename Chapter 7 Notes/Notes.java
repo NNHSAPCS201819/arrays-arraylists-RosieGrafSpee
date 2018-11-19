f3df0fa1ae7d5d9224611aea5b27f09f0945915b
RosieGrafSpee <36432935+RosieGrafSpee@users.noreply.github.com>
2018-11-19 15:09:41 -0600

    hellow

--- Chapter 7 Notes/Notes.java
+++ Chapter 7 Notes/Notes.java
@@ -7,4 +7,20 @@
  */
 public class Notes
 {
+    public static void CreatEvenArray()
+    {
+        int[] evens = new int[123];
+        
+        for (int i = 0; i < evens.length; i++)
+        {
+            evens[i] = 2 * i + 2;
+        }
+        
+        for (int i = 0; i < evens.length; i++)
+        {
+            System.out.println(evens[i]);
+        }
+        
+        System.out.println(evens);
+    }
 }
