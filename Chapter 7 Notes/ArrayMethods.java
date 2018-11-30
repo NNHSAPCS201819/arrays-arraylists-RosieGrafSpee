e62072ebaa9448703217f7be54812435f4139a7a
RosieGrafSpee <36432935+RosieGrafSpee@users.noreply.github.com>
2018-11-30 15:11:16 -0600

    dd

--- Chapter 7 Notes/ArrayMethods.java
+++ Chapter 7 Notes/ArrayMethods.java
@@ -29,8 +29,25 @@ public class ArrayMethods
 
    public static void main( String[] args )
    {
-      int[] testValues = { 3, 5, 6, 1 /* add your own values */ };
+      int[] testValues = { 3, 5, 6, 1289379284, 38472938, 89723819, 1782641726};
+      
+      for (int i = 0; i < testValues.length; i++)
+      {
+          if (testValues[i] % 2 == 0)
+          {
+              testValues[i] = 0;
+          }
+      }
       ArrayMethods tester = new ArrayMethods( testValues );
+      tester.printArray();
+      
+      int temp1 = 0, temp2 = 0;
+      for (int i = 1; i < testValues.length - 1; i++)
+      {
+          temp1 = temp2;
+          temp2 = testValues[i - 1] > testValues[i + 1] ? testValues[i - 1] : testValues[i + 1];
+      }
+
       tester.printArray();
       tester.swapFirstAndLast();
       tester.printArray();
